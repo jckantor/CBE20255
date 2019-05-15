2da512d33a4c8bfedddfc386132ffeee7566bf90
Jeffrey Kantor <kantor.1@nd.edu>
2019-05-14 21:42:34 -0400

    update formatting

--- tools/__main__.py
+++ tools/__main__.py
@@ -29,22 +29,23 @@ one or more problem statements with solutions.
 """
 
 README_FOOTER = """
-<p>**Note on the use of Python.** The Python used in these notebooks is deliberately limited to a core set of language 
+<p>
+**Note on the use of Python.** The Python used in these notebooks is deliberately limited to a core set of language 
 features.  These notebooks use scalar variables and lists of scalar variables to represent data. Also used are 
 arithmetic, math, print, and plotting functions from the `matplotlib.pyplot` library. Functions created with `def` 
 and `lambda` are used when root-finding calculations are required. List comprehesions are used on occasion when the 
 result is more readable code. The `Sympy` library for symbolic math is used extensively for writing mass balances. Other
  libraries included `numpy`, `math`, and the root-finding functions from `scipy.optimize`. Notebooks with more advanced 
- use of Python, such as dictionaries, are marked with an asterisk.</p>
+ use of Python, such as dictionaries, are marked with an asterisk.
 
-<p>**License Requirements.** The materials in this repository are available at 
+**License Requirements.** The materials in this repository are available at 
 [http://github.com/jckantor/CBE20255](http://github.com/jckantor/CBE20255) for noncommercial use under terms of the 
 [Creative Commons Attribution Noncommericial ShareAlike License](http://creativecommons.org/licenses/by-nc-sa/4.0/). 
 You are invited to fork this repository, and to use, adapt, remix these material for non-commericial purposes. The 
 license terms require you to give attribution and share your work under the same terms. Pull requests for corrections 
-and additions to these materials are most welcome.</p>
+and additions to these materials are most welcome.
 
-<p>**Acknowledgements.** Several notebooks embed videos from [LearnChemE](http://www.learncheme.com/) hosted at the 
+**Acknowledgements.** Several notebooks embed videos from [LearnChemE](http://www.learncheme.com/) hosted at the 
 University of Colorado at Boulder and sponsored by the National Science Foundation (NSF) and Shell Corporation. 
 Permission to use these videos is gratefully acknowledged.</p>
 """
